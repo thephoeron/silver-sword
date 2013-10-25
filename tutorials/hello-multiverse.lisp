069c454d08f3feadbbbec6aaa406a267e8487dae
the Phoeron <thephoeron@me.com>
2013-10-24 21:53:18 -0400

    Clean up reduntant setf's in hello-multiverse.lisp

--- tutorials/hello-multiverse.lisp
+++ tutorials/hello-multiverse.lisp
@@ -12,15 +12,15 @@
           (answer nil)) ;; define answer symbol for later
       ;; Define the energy program
       ; energies for 'h'
-      (setf (elt h 48) 0.5)
-      (setf (elt h 53) 0.5)
-      (setf (elt h 52) -0.5)
-      (setf (elt h 49) -0.1)
+      (setf (elt h 48) 0.5
+            (elt h 53) 0.5
+            (elt h 52) -0.5
+            (elt h 49) -0.1)
       ; couplers for 'J'
-      (setf (gethash "(48,53)" j) -0.5)
-      (setf (gethash "(48,52)" j) 0.2)
-      (setf (gethash "(52,49)" j) -0.3)
-      (setf (gethash "(53,49)" j) 0.8)
+      (setf (gethash "(48,53)" j) -0.5
+            (gethash "(48,52)" j) 0.2
+            (gethash "(52,49)" j) -0.3
+            (gethash "(53,49)" j) 0.8)
       ;; Solve the problem
       (setf answer (ss:solve-ising solver h j :num-reads 100 :max-answers 100))
       ;; Print the results
@@ -31,10 +31,10 @@
       (format t "~%Lowest Energy: ~S" (elt (gethash "energies" answer) 0))
       (format t "~%Number of solutions: ~S" (length (gethash "solutions" answer)))
       (format t "~%Best solution: ~S ~S ~S ~S"
-        (elt (elt (gethash "solutions" answer) 0) 56)
-        (elt (elt (gethash "solutions" answer) 0) 60)
-        (elt (elt (gethash "solutions" answer) 0) 57)
-        (elt (elt (gethash "solutions" answer) 0) 61))))
+        (elt (elt (gethash "solutions" answer) 0) 48)
+        (elt (elt (gethash "solutions" answer) 0) 49)
+        (elt (elt (gethash "solutions" answer) 0) 52)
+        (elt (elt (gethash "solutions" answer) 0) 53))))
 
 ;; after (ql:quickload "silver-sword-tutorials"), run (sst::hello-multiverse)
 ;; and watch it go!  run it a few times to compare your answers
